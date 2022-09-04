5ed6459b567929489a34401e74147637643b26c3
Brian-Machiestay <brianmachiestay@gmail.com>
2022-09-04 09:02:40 +0000

    writes a console for airbnb

--- console.py
+++ console.py
@@ -3,6 +3,9 @@
 
 import cmd
 import sys
+import json
+import os
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -21,6 +24,45 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         return
+
+    def do_create(self, arg):
+        """creates a new object and saves it"""
+        cmds = self.parseline(arg)
+        if cmds[2] == '':
+            print("** class name missing **")
+            return
+        list_of_cmds = cmds[2].split(" ")
+        if list_of_cmds[0] == "BaseModel":
+            new = BaseModel()
+            new.save()
+            print(new.id)
+        else:
+            print("** class doesn't exist **")
+
+    def do_show(self, arg):
+        """Prints the string representation
+        of an instance based on the class name and id
+        """
+        cmds = self.parseline(arg)
+        if cmds[2] == '':
+            print("** class name missing **")
+            return
+        list_of_cmds = cmds[2].split(" ")
+        lenth_of_cmds = len(list_of_cmds)
+        if list_of_cmds[0] != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        if lenth_of_cmds == 1:
+            print("** instance id missing **")
+            return
+        if os.path.exists("file.json"):
+            with open("file.json", "r", encoding="utf-8") as f:
+                dic = json.loads(f.read())
+                for key, value in dic.items():
+                    if (dic[key])['id'] == list_of_cmds[1]:
+                        print(BaseModel(**value))
+                        return
+        print("** no instance found **")
 
 
 if __name__ == '__main__':
